e20e79c6c77570699e42ba36a2e0b8a0362ab95b
Karl Marble <karlmarble@mail.weber.edu>
2016-09-29 12:50:20 -0600

    Added --help function

--- house_bolton_hw3.sh
+++ house_bolton_hw3.sh
@@ -8,11 +8,16 @@
 #        AUTHOR: Karl Marble, karlmarble@mail.weber.edu
 #  ORGANIZATION: 
 #       CREATED: 09/29/2016 12:17
-#      REVISION:  ---
+#      REVISION:  0.2
 #===============================================================================
 
 #set -o nounset                              # Treat unset variables as an error
 
+
+function reqHelp {
+	echo "Usage is house_bolton.sh [-s sedsrc] [-a awksrc] [-i inputFile]"
+	echo "All three arguments are required."
+}
 
 
 
