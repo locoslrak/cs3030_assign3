642162eee2dbef7dea41c8a44392b2120376c23a
Karl Marble <karlmarble@mail.weber.edu>
2016-09-29 12:20:50 -0600

    initial commit

---
+++ house_bolton_hw3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#         USAGE: ./bolton.hw3.sh 
+# 
+#   DESCRIPTION: 
+# 
+#        AUTHOR: Karl Marble, karlmarble@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 09/29/2016 12:17
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+
+
+exit 0
